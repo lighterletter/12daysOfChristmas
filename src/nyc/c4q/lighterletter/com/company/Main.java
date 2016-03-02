d898d5cb1ce1eed5f5cd884a22035909bdd010ae
John Gomez <jgomez818@gmail.com>
2016-03-01 22:07:16 -0500

    fixed and finished

--- src/nyc/c4q/lighterletter/com/company/Main.java
+++ src/nyc/c4q/lighterletter/com/company/Main.java
@@ -1,42 +1,60 @@
 package nyc.c4q.lighterletter.com.company;
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        String [] anArray;
-        anArray = new String[12];
+        String [] Arr = new String[12];
 
-        anArray[0]= "first";
-        anArray[1]= "second";
-        anArray[2]="third";
-        anArray[3]="fourth";
-        anArray[4]="fifth";
-        anArray[5]="sixth";
-        anArray[6]="seventh";
-        anArray[7]="eight";
-        anArray[8]="ninth";
-        anArray[9]="tenth";
-        anArray[10]="eleventh";
-        anArray[11]="twelfth";
+        Arr[0]= "first";
+        Arr[1]= "second";
+        Arr[2]= "third";
+        Arr[3]= "fourth";
+        Arr[4]= "fifth";
+        Arr[5]= "sixth";
+        Arr[6]= "seventh";
+        Arr[7]= "eight";
+        Arr[8]= "ninth";
+        Arr[9]= "tenth";
+        Arr[10]= "eleventh";
+        Arr[11]= "twelfth";
+
+        HashMap<String, String> map = new HashMap<String, String>();
+
+        map.put("first","A Partridge in a Pear Tree");
+        map.put("second","Two Turtle Doves");
+        map.put("third","Three French Hens");
+        map.put("fourth","Four Calling Birds");
+        map.put("fifth","Five Golden Rings");
+        map.put("sixth","Six Geese a-Laying");
+        map.put("seventh","Seven Swans a-Swimming");
+        map.put("eight","Eight Maids a-Milking");
+        map.put("ninth","Nine Ladies Dancing");
+        map.put("tenth","Ten Lords a-Leaping");
+        map.put("eleventh","Eleven Pipers Piping");
+        map.put("twelfth","Twelve Drummers Drumming");
 
 
-        String dayOf = "first";
-        String verse = "On the " + anArray + "day of Christmas my true love gave to me;";
-        String verse2 = "day of Christmas my true love gave to me;";
+        System.out.println("On the first day of Christmas my true love gave to me;");
+        System.out.println(map.get("first"));
+        System.out.println();
 
-        String gift = "And a piper in a pear tree.";
-        String sentence = verse + gift;
-        for (i = 0; i < 12; i = i + 1) {
+        for (int i = 1; i < 12; i++) {
 
+            String verse = "On the " + Arr[i] + " day of Christmas my true love gave to me;";
+            System.out.println(verse);
 
-            System.out.printf(sentence);
-            System.out.println();
-            /*Scanner input = new Scanner(System.in);
-            System.out.println("Enter a line of text:");
-            String line = input.nextLine();
-            System.out.println("You entered:" + line);*/
+                for (int j = i; j >= 0; j--) {
+
+                    if(j == 0){
+                        System.out.println("and "+map.get(Arr[j]));
+                        System.out.println();
+                    } else
+                    System.out.println(""+map.get(Arr[j]));
+                }
         }
     }
 }
